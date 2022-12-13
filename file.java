ad6af20b8c67135ac2146706a1aa42de57e21ad7
Java codes <vermashubhanshu541@gmail.com>
2022-12-13 23:44:26 +0530

    After modification

---
+++ file.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Modified");
+        System.out.println("Just a demo file to learn about git");
+    }
+}
